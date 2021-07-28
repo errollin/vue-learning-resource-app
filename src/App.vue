6a305f8ce6c9262c272acc94914fb3fd2a276577
ErrolLin <errolriccolin@foxmail.com>
2021-07-28 11:49:50 +0800

    Add TheHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
+  <the-header title="RememberMe"></the-header>
   <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
 import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
 
 export default {
   name: 'App',
   components: {
     StoredResources,
+    TheHeader,
   },
   data() {
     return {
